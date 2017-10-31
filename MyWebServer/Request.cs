799be6acfa2f8e07ea753a2cd00df3de5f2651b0
Stefan Ilic <stefan.ilic@technikum-wien.at>
2017-10-31 10:56:15 +0100

    Rewrite requestline handling

--- MyWebServer/Request.cs
+++ MyWebServer/Request.cs
@@ -10,35 +10,39 @@ namespace MyWebServer
 {
     public class Request : IRequest
     {
-        private readonly Regex _requestPattern = new Regex(@"^(GET|POST|PUT|DELETE|get|post|put|delete)(\s+)\/(.*)(\s+)(HTTP\/)([0-9].[0-9]).*");
-
+        private readonly Regex _requestPattern = new Regex(@"^(GET|POST|PUT|DELETE|get|post|put|delete)(\s+)\/(.*)(\s+)(HTTP\/)(1\.[0-9])$");
         public Request(Stream requestStream)
         {
             using (var sr = new StreamReader(requestStream))
             {
-                IsValid = _requestPattern.IsMatch(sr.ReadToEnd());
-                if (IsValid)
+                var firstline = sr.ReadLine();
+                if (firstline == null)
                 {
-                    requestStream.Seek(0, SeekOrigin.Begin);
-                    var requestLine = sr.ReadLine().Split(' ');
-                    Method = requestLine[0].ToUpper();
-                    Url = new Url(requestLine[1]);
+                    return;
+                }
+                IsValid = _requestPattern.IsMatch(firstline);
+                if (!IsValid)
+                {
+                    return;
+                }
+                var requestLine = firstline.Split(' ');
+                Method = requestLine[0].ToUpper();
+                Url = new Url(requestLine[1]);
 
-                    while (sr.Peek() >= 0)
+                while (sr.Peek() >= 0)
+                {
+                    var tempSplit = sr.ReadLine().Split(' ');
+                    var tempKey = tempSplit.Length == 2 ? tempSplit[0].TrimEnd(':').ToLower() : "";
+                    var tempVal = tempSplit.Length == 2 ? tempSplit[1].TrimEnd('\r', '\n') : "";
+                    if (tempKey != string.Empty && tempVal != string.Empty)
                     {
-                        var tempSplit = sr.ReadLine().Split(' ');
-                        var tempKey = tempSplit.Length == 2 ? tempSplit[0].TrimEnd(':').ToLower() : "";
-                        var tempVal = tempSplit.Length == 2 ? tempSplit[1].TrimEnd('\r', '\n') : "";
-                        if (tempKey != String.Empty && tempVal != String.Empty)
-                        {
-                            Headers.Add(tempKey, tempVal);
-                        }
+                        Headers.Add(tempKey, tempVal);
                     }
                 }
             }
         }
 
-        public bool IsValid { get; }
+        public bool IsValid { get; } = false;
         public string Method { get; }
         public IUrl Url { get; } = new Url();
         public IDictionary<string, string> Headers { get; } = new Dictionary<string, string>();
@@ -47,7 +51,7 @@ namespace MyWebServer
         public int ContentLength { get; }
         public string ContentType { get; }
         public Stream ContentStream { get; }
-        public string ContentString { get; } = String.Empty;
+        public string ContentString { get; } = string.Empty;
         public byte[] ContentBytes { get; }
     }
 }

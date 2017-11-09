f3c9581c1baef054d4196079c90eebe62e21a00f
Stefan Ilic <stefan.ilic.vie@gmail.com>
2017-11-09 14:18:58 +0100

    Implement PluginManager with empty methods

---
+++ MyWebServer/PluginManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using BIF.SWE1.Interfaces;
+
+namespace MyWebServer
+{
+    class PluginManager : IPluginManager
+    {
+
+        public void Add(IPlugin plugin)
+        {
+            
+        }
+
+        public void Add(string plugin)
+        {
+            
+        }
+
+        public void Clear()
+        {
+            
+        }
+
+        public IEnumerable<IPlugin> Plugins { get; } = new List<IPlugin>();
+    }
+}

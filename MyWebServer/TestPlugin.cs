61a28f663f25bd9ba5f02dc53bdb3156b31dfa63
Stefan Ilic <stefan.ilic.vie@gmail.com>
2017-11-11 20:40:19 +0100

    Finish UE3

---
+++ MyWebServer/TestPlugin.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using BIF.SWE1.Interfaces;
+
+namespace MyWebServer
+{
+    public class TestPlugin : IPlugin
+    {
+        public float CanHandle(IRequest req)
+        {
+            var url = req.Url.RawUrl.ToLower();
+            if (url == "/")
+            {
+                return 1;
+            }
+            return Regex.Matches(req.Url.RawUrl.ToLower(), "test").Count * 0.01f;
+        }
+
+        public IResponse Handle(IRequest req)
+        {
+            var obj = new Response
+            {
+                StatusCode = 200
+            };
+            obj.SetContent("I wear a jacket indoors because man's not hot");
+            return obj;
+        }
+    }
+}

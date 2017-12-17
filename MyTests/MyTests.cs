51db878ef1ecb32f3778c1974d2ab10b52186342
Stefan Ilic <stefan.ilic.vie@gmail.com>
2017-12-17 14:54:40 +0100

    Fix MyTests

--- MyTests/MyTests.cs
+++ MyTests/MyTests.cs
@@ -1,52 +1,28 @@
 ﻿using System.IO;
 using System.Text;
 using BIF.SWE1.Interfaces;
-using BIF.SWE1.UnitTests;
+using MyWebServer;
 using NUnit.Framework;
 
 namespace MyTests
 {
     [TestFixture]
-    public class MyTests : AbstractTestFixture<IMyTests>
+    public class MyTests
     {
-        [Test]
-        public void HelloWorld()
-        {
-            var ueb = CreateInstance();
-            ueb.HelloWorld();
-        }
-
         #region ToLowerPlugin
-
-        private static StringBuilder GetBody(IResponse resp)
-        {
-            StringBuilder body = new StringBuilder();
-            using (var ms = new MemoryStream())
-            {
-                resp.Send(ms);
-                ms.Seek(0, SeekOrigin.Begin);
-                var sr = new StreamReader(ms);
-                while (!sr.EndOfStream)
-                {
-                    body.AppendLine(sr.ReadLine());
-                }
-            }
-            return body;
-        }
 
         [Test]
         public void tolower_handle_big_input()
         {
-            var ueb = CreateInstance();
-            var plugin = ueb.GetToLowerPlugin();
+            var plugin = GetToLowerPlugin();
             Assert.That(plugin, Is.Not.Null, "IUEB6.GetToLowerPlugin returned null");
 
-            var url = ueb.GetToLowerUrl();
+            var url = GetToLowerUrl();
             Assert.That(url, Is.Not.Null, "IUEB6.GetToLowerUrl returned null");
 
             var textToTest = new string('*', 1000);
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
+            var req = GetRequest(GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
             Assert.That(req, Is.Not.Null, "IUEB6.GetRequest returned null");
 
             Assert.That(plugin.CanHandle(req), Is.GreaterThan(0).And.LessThanOrEqualTo(1));
@@ -63,16 +39,15 @@ namespace MyTests
         [Test]
         public void tolower_handle_spaces()
         {
-            var ueb = CreateInstance();
-            var plugin = ueb.GetToLowerPlugin();
+            var plugin = GetToLowerPlugin();
             Assert.That(plugin, Is.Not.Null, "IUEB6.GetToLowerPlugin returned null");
 
-            var url = ueb.GetToLowerUrl();
+            var url = GetToLowerUrl();
             Assert.That(url, Is.Not.Null, "IUEB6.GetToLowerUrl returned null");
 
             var textToTest = "I contain lots and lots of spaces";
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
+            var req = GetRequest(GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
             Assert.That(req, Is.Not.Null, "IUEB6.GetRequest returned null");
 
             Assert.That(plugin.CanHandle(req), Is.GreaterThan(0).And.LessThanOrEqualTo(1));
@@ -89,16 +64,15 @@ namespace MyTests
         [Test]
         public void tolower_handle_umlauts()
         {
-            var ueb = CreateInstance();
-            var plugin = ueb.GetToLowerPlugin();
+            var plugin = GetToLowerPlugin();
             Assert.That(plugin, Is.Not.Null, "IUEB6.GetToLowerPlugin returned null");
 
-            var url = ueb.GetToLowerUrl();
+            var url = GetToLowerUrl();
             Assert.That(url, Is.Not.Null, "IUEB6.GetToLowerUrl returned null");
 
             var textToTest = "ÜÜÜ ÄÄÄ ÖÖÖ";
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
+            var req = GetRequest(GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
             Assert.That(req, Is.Not.Null, "IUEB6.GetRequest returned null");
 
             Assert.That(plugin.CanHandle(req), Is.GreaterThan(0).And.LessThanOrEqualTo(1));
@@ -115,16 +89,15 @@ namespace MyTests
         [Test]
         public void tolower_handle_cyrillic()
         {
-            var ueb = CreateInstance();
-            var plugin = ueb.GetToLowerPlugin();
+            var plugin = GetToLowerPlugin();
             Assert.That(plugin, Is.Not.Null, "IUEB6.GetToLowerPlugin returned null");
 
-            var url = ueb.GetToLowerUrl();
+            var url = GetToLowerUrl();
             Assert.That(url, Is.Not.Null, "IUEB6.GetToLowerUrl returned null");
 
             var textToTest = "Это русская строка";
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
+            var req = GetRequest(GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
             Assert.That(req, Is.Not.Null, "IUEB6.GetRequest returned null");
 
             Assert.That(plugin.CanHandle(req), Is.GreaterThan(0).And.LessThanOrEqualTo(1));
@@ -141,16 +114,15 @@ namespace MyTests
         [Test]
         public void tolower_handle_html()
         {
-            var ueb = CreateInstance();
-            var plugin = ueb.GetToLowerPlugin();
+            var plugin = GetToLowerPlugin();
             Assert.That(plugin, Is.Not.Null, "IUEB6.GetToLowerPlugin returned null");
 
-            var url = ueb.GetToLowerUrl();
+            var url = GetToLowerUrl();
             Assert.That(url, Is.Not.Null, "IUEB6.GetToLowerUrl returned null");
 
             var textToTest = "<script> I am a cross site script </script>";
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
+            var req = GetRequest(GetValidRequestStream(url, method: "POST", body: $"text={textToTest}"));
             Assert.That(req, Is.Not.Null, "IUEB6.GetRequest returned null");
 
             Assert.That(plugin.CanHandle(req), Is.GreaterThan(0).And.LessThanOrEqualTo(1));
@@ -170,14 +142,13 @@ namespace MyTests
         [Test]
         public void static_handle_txt()
         {
-            var ueb = CreateInstance();
-            var obj = ueb.GetStaticFilePlugin();
+            var obj = GetStaticFilePlugin();
             Assert.That(obj, Is.Not.Null, "IUEB5.GetStaticFilePlugin returned null");
 
             var url = @"C:\projects\SWE1\SWE1-CS\deploy\static\test.txt";
             Assert.That(url, Is.Not.Null, "IUEB5.GetStaticFileUrl returned null");
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url));
+            var req = GetRequest(GetValidRequestStream(url));
             Assert.That(req, Is.Not.Null, "IUEB5.GetRequest returned null");
 
             Assert.That(obj.CanHandle(req), Is.GreaterThan(0.0f).And.LessThanOrEqualTo(1.0f));
@@ -191,14 +162,13 @@ namespace MyTests
         [Test]
         public void static_handle_jpg()
         {
-            var ueb = CreateInstance();
-            var obj = ueb.GetStaticFilePlugin();
+            var obj = GetStaticFilePlugin();
             Assert.That(obj, Is.Not.Null, "IUEB5.GetStaticFilePlugin returned null");
 
             var url = @"C:\projects\SWE1\SWE1-CS\deploy\static\test.jpg";
             Assert.That(url, Is.Not.Null, "IUEB5.GetStaticFileUrl returned null");
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url));
+            var req = GetRequest(GetValidRequestStream(url));
             Assert.That(req, Is.Not.Null, "IUEB5.GetRequest returned null");
 
             Assert.That(obj.CanHandle(req), Is.GreaterThan(0.0f).And.LessThanOrEqualTo(1.0f));
@@ -213,14 +183,13 @@ namespace MyTests
         [Test]
         public void static_handle_png()
         {
-            var ueb = CreateInstance();
-            var obj = ueb.GetStaticFilePlugin();
+            var obj = GetStaticFilePlugin();
             Assert.That(obj, Is.Not.Null, "IUEB5.GetStaticFilePlugin returned null");
 
             var url = @"C:\projects\SWE1\SWE1-CS\deploy\static\test.png";
             Assert.That(url, Is.Not.Null, "IUEB5.GetStaticFileUrl returned null");
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url));
+            var req = GetRequest(GetValidRequestStream(url));
             Assert.That(req, Is.Not.Null, "IUEB5.GetRequest returned null");
 
             Assert.That(obj.CanHandle(req), Is.GreaterThan(0.0f).And.LessThanOrEqualTo(1.0f));
@@ -234,14 +203,13 @@ namespace MyTests
         [Test]
         public void static_handle_pdf()
         {
-            var ueb = CreateInstance();
-            var obj = ueb.GetStaticFilePlugin();
+            var obj = GetStaticFilePlugin();
             Assert.That(obj, Is.Not.Null, "IUEB5.GetStaticFilePlugin returned null");
 
             var url = @"C:\projects\SWE1\SWE1-CS\deploy\static\test.pdf";
             Assert.That(url, Is.Not.Null, "IUEB5.GetStaticFileUrl returned null");
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url));
+            var req = GetRequest(GetValidRequestStream(url));
             Assert.That(req, Is.Not.Null, "IUEB5.GetRequest returned null");
 
             Assert.That(obj.CanHandle(req), Is.GreaterThan(0.0f).And.LessThanOrEqualTo(1.0f));
@@ -255,14 +223,13 @@ namespace MyTests
         [Test]
         public void static_handle_html()
         {
-            var ueb = CreateInstance();
-            var obj = ueb.GetStaticFilePlugin();
+            var obj = GetStaticFilePlugin();
             Assert.That(obj, Is.Not.Null, "IUEB5.GetStaticFilePlugin returned null");
 
             var url = @"C:\projects\SWE1\SWE1-CS\deploy\static\test.html";
             Assert.That(url, Is.Not.Null, "IUEB5.GetStaticFileUrl returned null");
 
-            var req = ueb.GetRequest(RequestHelper.GetValidRequestStream(url));
+            var req = GetRequest(GetValidRequestStream(url));
             Assert.That(req, Is.Not.Null, "IUEB5.GetRequest returned null");
 
             Assert.That(obj.CanHandle(req), Is.GreaterThan(0.0f).And.LessThanOrEqualTo(1.0f));
@@ -272,6 +239,120 @@ namespace MyTests
             Assert.That(resp.Headers["content-type"], Is.Not.Null);
             Assert.That(resp.Headers["content-type"], Is.EqualTo("text/html"));
         }
+        #endregion
+
+        #region Helper
+
+        public static Stream GetValidRequestStream(string url, string method = "GET", string host = "localhost", string[][] header = null, string body = null)
+        {
+            byte[] bodyBytes = null;
+            if (body != null)
+            {
+                bodyBytes = Encoding.UTF8.GetBytes(body);
+            }
+
+            var ms = new MemoryStream();
+            var sw = new StreamWriter(ms, Encoding.ASCII);
+
+            sw.WriteLine("{0} {1} HTTP/1.1", method, url);
+            sw.WriteLine("Host: {0}", host);
+            sw.WriteLine("Connection: keep-alive");
+            sw.WriteLine("Accept: text/html,application/xhtml+xml");
+            sw.WriteLine("User-Agent: Unit-Test-Agent/1.0 (The OS)");
+            sw.WriteLine("Accept-Encoding: gzip,deflate,sdch");
+            sw.WriteLine("Accept-Language: de-AT,de;q=0.8,en-US;q=0.6,en;q=0.4");
+            if (bodyBytes != null)
+            {
+                sw.WriteLine(string.Format("Content-Length: {0}", bodyBytes.Length));
+                sw.WriteLine("Content-Type: application/x-www-form-urlencoded");
+            }
+            if (header != null)
+            {
+                foreach (var h in header)
+                {
+                    sw.WriteLine(string.Format("{0}: {1}", h[0], h[1]));
+                }
+            }
+            sw.WriteLine();
+
+            if (bodyBytes != null)
+            {
+                sw.Flush();
+                ms.Write(bodyBytes, 0, bodyBytes.Length);
+            }
+
+            sw.Flush();
+
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms;
+        }
+
+        public static Stream GetInvalidRequestStream()
+        {
+            var ms = new MemoryStream();
+            var sw = new StreamWriter(ms, Encoding.ASCII);
+
+            sw.WriteLine("GET");
+            sw.WriteLine();
+            sw.Flush();
+
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms;
+        }
+
+        public static Stream GetEmptyRequestStream()
+        {
+            var ms = new MemoryStream();
+            var sw = new StreamWriter(ms, Encoding.ASCII);
+
+            sw.WriteLine();
+            sw.Flush();
+
+            ms.Seek(0, SeekOrigin.Begin);
+            return ms;
+        }
+
+        private static StringBuilder GetBody(IResponse resp)
+        {
+            StringBuilder body = new StringBuilder();
+            using (var ms = new MemoryStream())
+            {
+                resp.Send(ms);
+                ms.Seek(0, SeekOrigin.Begin);
+                var sr = new StreamReader(ms);
+                while (!sr.EndOfStream)
+                {
+                    body.AppendLine(sr.ReadLine());
+                }
+            }
+            return body;
+        }
+
+        public void HelloWorld()
+        {
+            // I'm fine
+        }
+
+        public IRequest GetRequest(System.IO.Stream network)
+        {
+            return new Request(network);
+        }
+
+        public IPlugin GetToLowerPlugin()
+        {
+            return new ToLowerPlugin();
+        }
+
+        public string GetToLowerUrl()
+        {
+            return "/tolower";
+        }
+
+        public IPlugin GetStaticFilePlugin()
+        {
+            return new StaticFilePlugin();
+        }
+
         #endregion
     }
 }
